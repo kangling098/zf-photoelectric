6ed4e6beb53260b15aa5f8ea51648b3f9860d802
kangling098 <461060819@qq.com>
2017-07-16 23:02:32 +0800

    xinzeng

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+  	<commonHeader></commonHeader>
+    <div class="banner">
+    	<div class="banner-bg">
+    		<ul class="banner-nav">
+	    		<template v-for="(item,index) in banner.list">
+	    			<li class="banner-item item-1">{{index}}</li>
+    			</template>
+    		</ul>
+    	</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import commonHeader from './component/commonHeader.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      banner:{
+      	list:[
+      		{name:"p1"},
+      		{name:"p2"},
+      		{name:"p3"},
+      		{name:"p4"},
+      		{name:"p5"}
+      	]
+      }
+    }
+  },
+  components: { commonHeader }
+}
+</script>
+
+<style>
+#app {
+	min-width: 1200px;
+	margin: 0 auto;
+}
+.banner {
+	
+}
+</style>
---
+++ src/component/commonHeader.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="commonHeader">
+    <div class="nav-header">
+    	<a href="/" class="logo-bg">
+    		
+    	</a>
+    	<ul class="nav-center">
+    		<template v-for="(item,index) in navlist">
+    			<li class="nav-item-wrapper">
+    				<a class="nav-item" :href="item.url">{{item.name}}</a>
+    			</li>
+    		</template>
+    	</ul>
+    	<div class="nav-right">
+    		<a class="change-language-a chinese-change active" href="javascript:;">中文</a>&nbsp;|&nbsp;
+    		<a class="change-language-a english-change" href="javascript:;">English</a>
+    	</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: "微信公众号 jinkey-love",
+      navlist:[
+      	{
+      		name:"首页",
+      		url:"/"
+      	},{
+      		name:"关于兆丰",
+      		url:"/about/"
+      	},{
+      		name:"产品展示",
+      		url:"/production/"
+      	},{
+      		name:"用户服务",
+      		url:"/service/"
+      	},{
+      		name:"人才招聘",
+      		url:"/join/"
+      	}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.nav-header {
+	position: relative;
+	margin: 0 auto;
+	width: 100%;
+	min-width: 1200px;
+	height: 99px;
+	background: url(../css/img/common-header-bg.jpg) repeat-x;
+}
+.logo-bg {
+	position: absolute;
+	left: 10%;
+	top: 20px;
+	display: block;
+	width: 100px;
+	height: 50px;
+	background: #0080ff;
+}
+.nav-center {
+	margin: 0 auto;
+	width: 50%;
+	height: 100%;
+}
+.nav-item-wrapper {
+	float: left;
+	width: 20%;
+	height: 100%;
+	line-height: 99px;
+	text-align: center;
+}
+.nav-item {
+	display: inline-block;
+	height: 74px;
+	border-bottom: 3px solid transparent;
+	color: #333;
+	font-size: 24px;
+}
+.nav-item:hover,.nav-item.active {
+	border-bottom-color: #ee7c17;
+}
+.nav-right {
+	position: absolute;
+	right: 0;
+	top: 50px;
+	width: 120px;
+	height: 24px;
+	line-height: 24px;
+	
+}
+.change-language-a {
+	font-size: 18px;
+	color: #474444;
+	
+}
+.change-language-a.active {
+	color: #ee7c17;
+}
+</style>
